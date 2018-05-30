ff6fbd926910259d9f19bc95c42176cc718c31c7
Aditya Jawade <adityajawade93@gmail.com>
2018-05-30 16:44:01 +0530

    add link to app header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import 'whatwg-fetch'
 import Main from '../Main'
+import { Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">TV Series List</h1>
+          <h1 className="App-title"><Link to={'/'}>TV Series List</Link></h1>
         </header>
         <Main />
       </div>    
